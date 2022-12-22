0a85517eb6edfe8c934fa88dc0526de2b5b68d40
GutoRomagnolo <guromagnolo23@hotmail.com>
2022-12-22 20:17:41 -0300

    feat: implementando camadas client e server

---
+++ client/customers-list/customers-list.js
@@ -0,0 +1,79 @@
+const studentListContainer = document.getElementById("customers-list");
+let studentsRegister = [];
+
+const getStudentsRegister = async () => {
+  try {
+    const response = await fetch("./customers-register.json");
+    const disorderlyStudentsRegister = await response.json();
+    disorderlyStudentsRegister.forEach(student => student.identifier = Number(student.identifier))
+  
+    studentsRegister = sortStudentsRegisterByIdentifer(disorderlyStudentsRegister);
+    generateRegisterList();
+  } catch (error) {
+    studentListContainer.innerHTML += `
+      <p class="registers-not-found">Nenhum registro encontrado :(</p>
+    `
+  }
+};
+
+const generateRegisterList = () => {
+  studentsRegister.forEach((student) => {
+    studentListContainer.innerHTML += 
+      `<div class="register-container">
+        <div class="standard-container">
+          <h3>NOME</h3>
+          <h4>${student.name}</h4>
+          <hr class="horizontal-line">
+        </div>
+        <div class="standard-container about-container">
+          <div class="three-row">
+            <h3>RA</h3>
+            <h4>${student.identifier}</h4>
+            <hr class="horizontal-line">
+          </div>
+          <div class="three-row">
+            <h3>GÊNERO</h3>
+            <h4>${student.gender}</h4>
+            <hr class="horizontal-line">
+          </div>
+          <div class="three-row">
+            <h3>IDADE</h3>
+            <h4>${student.age}</h4>
+            <hr class="horizontal-line">
+          </div>
+        </div>
+        <div class="standard-container">
+          <div>
+            <h3>E-MAIL</h3>
+            <h4>${student.email}</h4>
+            <hr class="horizontal-line">
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>ENDEREÇO</h3>
+            <h4>${student.address}</h4>
+            <hr class="horizontal-line">
+          </div>
+        </div>
+      </div>`;
+  });
+};
+
+const sortStudentsRegisterByIdentifer = studentsRegisterToSort => {
+  return studentsRegisterToSort.sort((nextStudent, previousStudent) => {
+    if (nextStudent.identifier > previousStudent.identifier) {
+      return 1;
+    } else if (nextStudent.identifier === previousStudent.identifier) {
+      if (nextStudent.name > previousStudent.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      return -1;
+    }
+  });
+}
+
+getStudentsRegister();
---
+++ client/customers-registration/customers-registration.js
@@ -0,0 +1,55 @@
+const registerForm = document.getElementById('register-form');
+const formInputs = Array.from(document.querySelectorAll('input'));
+const genderSelector = document.getElementById('select-gender');
+const submitButton = document.getElementById('submit-button');
+const mainContainer = document.getElementById('main-container');
+
+const submitRegisterForm = async (event) => {
+  event.preventDefault();
+  try {
+    await fetch('customers-registration.php', {
+      method: 'POST',
+      body: new FormData(registerForm)
+    });
+
+    const successMessage = 'Cliente registrado com sucesso!';
+    invokeRegisterResultMessage('success', successMessage);
+    registerForm.reset();
+    startElementsListeners();
+  } catch (error) {
+    const errorMessage = 'Não foi possível cadastrar o cliente!';
+    invokeRegisterResultMessage('fail', errorMessage);
+    console.log("Um erro ocorreu ao cadastrar cliente: ", error);
+  }
+};
+
+const validateFormInputs = () => {
+  const validInputs = formInputs.every(input => input.value)
+  const formIsValid = validInputs && genderSelector.options[genderSelector.selectedIndex].value
+  formIsValid
+    ? submitButton.removeAttribute('disabled')
+    : submitButton.setAttribute('disabled', 'disabled')
+}
+
+const invokeRegisterResultMessage = (result, resultMessage) => {
+  registerForm.innerHTML += `
+    <div class="success-message" id="success-message">
+      <h4>${resultMessage}</h4>
+      <img class="success-icon" src="./../assets/${result}-icon.svg">
+    </div>
+  `
+  setTimeout(() => {
+    const successMessage = document.getElementById('success-message')
+    successMessage.style.display = 'none';
+  }, 2200)
+}
+
+const startElementsListeners = () => {
+  formInputs.forEach(input => {
+    input.addEventListener('input', event => validateFormInputs());
+  })
+  
+  genderSelector.addEventListener('input', event => validateFormInputs());
+}
+
+startElementsListeners();
